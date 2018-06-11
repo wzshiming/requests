9aac2e446aaf5f2d2f35f1aedf39bf15412901e1
wzshiming <wzshiming@foxmail.com>
2018-06-11 16:32:28 +0800

    update demo

--- util/translate/google_translate.go
+++ util/translate/google_translate.go
@@ -39,27 +39,40 @@ func GoogleTranslate(text, sourcelang, targetlang string) (string, error) {
 	// [[["Hello there","你好",null,null,1]],null,"zh-CN",null,null,null,1,null,[["zh-CN"],null,[1],["zh-CN"]]]
 	body := resp.Body()
 
+	ret := messages{}
+	err = json.Unmarshal(body, &ret)
+	if err != nil {
+		return "", err
+	}
+	return ret.String(), nil
+}
+
+type messages []string
+
+func (m *messages) String() string {
+	text := strings.Join([]string(*m), " ")
+	text = strings.Replace(text, "\\n", "\n", -1)
+	return text
+}
+
+func (m *messages) UnmarshalJSON(body []byte) error {
 	rms := []json.RawMessage{}
 	for i := 0; i != 2; i++ {
 		err := json.Unmarshal(body, &rms)
 		if err != nil {
-			return "", err
+			return err
 		}
 		body = []byte(rms[0])
 	}
-
-	ret := []string{}
 	for _, v := range rms {
 		b := []json.RawMessage{}
 		err := json.Unmarshal([]byte(v), &b)
 		if err != nil {
-			return "", err
+			return err
 		}
 		by := b[0]
 		by = by[1 : len(by)-1]
-		ret = append(ret, string(by))
+		*m = append(*m, string(by))
 	}
-	text = strings.Join(ret, " ")
-	text = strings.Replace(text, "\\n", "\n", -1)
-	return text, nil
+	return nil
 }
